a04ef323aa632fec64b4e2babcb6b90a2d2ac59b
Ticiano <ticiano.damasceno@hotmail.com>
2024-11-24 13:10:48 -0300

    Concluindo todas as rodas

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
 import os
 import sys
+from datetime import datetime
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from sqlalchemy.sql import text
+from sqlite3 import IntegrityError
 from src.models.database.connection import db_connection_handler
 from src.models.database.repository import RepositoryUsuario, RepositoryFuncao,RepositorySala,RepositoryInventory
 from src.models.password_hash import HandlerPasswordHash
-from sqlalchemy.sql import text
 
 app = Flask(__name__)
 CORS(app)
@@ -43,6 +45,37 @@ with db_connection_handler as db:
 def main():
     return "Hello World"
 
+###### LOGIN ######
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"error": "Formato inválido. Necessário o parâmetro Header = Content-Type: application/json."}), 400
+    
+    dados_usuario = request.json
+
+    if len(dados_usuario) != 2:
+        return jsonify({"error": "Formato de payload inválido. Necessário 2 campos no payload (NM_USUARIO, SENHA)."}), 
+
+    if not dados_usuario.get('NM_USUARIO') or not dados_usuario.get('SENHA'):
+        list = [x for x in ('NM_USUARIO','SENHA') if x not in dados_usuario]
+
+        return jsonify({"error": "Formato de payload inválido. Campos faltantes: {}".format(list)}), 400
+
+    with db_connection_handler as db:
+        dados_usuario_base = db.execute(repository_usuario.select_user(dados_usuario['NM_USUARIO'])).fetchall()
+    
+    if len(dados_usuario_base) == 0:
+        return {'Error': 'Usuário ou senha inválido.'}, 404
+    elif not handler_password.verificar_senha(str(dados_usuario['SENHA']).encode('utf-8'),dados_usuario_base[0][1].encode('utf-8')):
+        return {'Error': 'Usuário ou senha inválido.'}, 404
+    elif handler_password.verificar_senha(str(dados_usuario['SENHA']).encode('utf-8'),dados_usuario_base[0][1].encode('utf-8')):
+        return jsonify({
+            'status':'Usuário logado com sucesso!', 
+            'usuario': dados_usuario_base[0][0], 
+            'NVL_ACESSO': dados_usuario_base[0][2]
+        }), 200 
+
+###### USUÁRIOS ######
 @app.route('/usuarios', methods=['GET'])
 def usuarios():
     with db_connection_handler as db:
@@ -75,11 +108,23 @@ def adicionar_usuario():
     dados_usuario['SENHA'] = handler_password.gerar_hash(str(dados_usuario['SENHA']).encode('utf-8')).decode('utf-8')
 
     with db_connection_handler as db:
-        db.begin() if not db.in_transaction() else None
-        db.execute(repository_usuario.insert_user(dados_usuario))
-        db.commit()
+        try:
+            db.begin() if not db.in_transaction() else None
+            db.execute(repository_usuario.insert_user(dados_usuario))
+            db.commit()
+        except Exception as e:
+            print(e)
+            db.rollback()
+            return {'Error': 'Houve um erro no banco de dados.'}, 400
         
-    return jsonify({'message': 'Usuário cadastrado com sucesso!', 'dados': dados_usuario}), 200
+    return jsonify({
+        'message': 'Usuário cadastrado com sucesso!',
+        'dados': {
+            'CPF':dados_usuario['CPF'], 
+            'NOME':dados_usuario['NM_USUARIO'],
+            'ID_FUNCAO':dados_usuario['ID_FUNCAO']
+        }
+    }), 200
 
 @app.route('/usuarios/alter/<id>', methods=['POST'])
 def alterar_usuario(id):
@@ -88,26 +133,51 @@ def alterar_usuario(id):
     
     dados_usuario = request.json
 
-    if len(dados_usuario) != 3:
-        return jsonify({"error": "Formato de payload inválido. Necessário 3 campos no payload (NM_USUARIO, CPF, ID_FUNCAO)."}), 400
+    if len(dados_usuario) not in (3,4) :
+        return jsonify({"error": "Formato de payload inválido. Necessário 3 campos no payload (NM_USUARIO, CPF, ID_FUNCAO, *SENHA). A senha é facultativa."}), 400
     
     if not dados_usuario.get('NM_USUARIO') or not dados_usuario.get('CPF') or not dados_usuario.get('ID_FUNCAO'):
         list = [x for x in ('NM_USUARIO','CPF','ID_FUNCAO') if x not in dados_usuario]
 
         return jsonify({"error": "Formato de payload inválido. Campos faltantes: {}".format(list)}), 400
 
+    dados_usuario['ALTERED_AT'] = datetime.now()
+    dados_usuario['SENHA'] = handler_password.gerar_hash(str(dados_usuario['SENHA']).encode('utf-8')).decode('utf-8') if dados_usuario.get('SENHA') else None
     dados_usuario['ID'] = id
 
     with db_connection_handler as db:
-        db.begin() if not db.in_transaction() else None
-        result = db.execute(repository_usuario.alter_user(dados_usuario))
-        db.commit()
+        try:
+            db.begin() if not db.in_transaction() else None
+            result = db.execute(repository_usuario.alter_user(dados_usuario))
+            db.commit()
+        except Exception as e:
+            print(e)
+            db.rollback()
+            return {'Error': 'Houve um erro no banco de dados.'}, 400
     
     if result.rowcount == 0:
         return jsonify({'error': 'ID_USUARIO não encontrado.', 'dados': dados_usuario}), 404
     else:
         return jsonify({'message': 'Usuário alterado com sucesso!', 'dados': dados_usuario}), 200
 
+@app.route('/usuarios/delete/<id>', methods=['POST'])
+def deletar_usuario(id):
+    with db_connection_handler as db:
+        try:
+            db.begin() if not db.in_transaction() else None
+            result = db.execute(repository_usuario.delete_user(id))
+            db.commit()
+        except Exception as e:
+            print(e)
+            db.rollback()
+            return {'Error': 'Houve um erro no banco de dados.'}, 400
+    
+    if result.rowcount == 0:
+        return jsonify({'error': f'ID_USUARIO {id} não encontrado.'}), 404
+    else:
+        return jsonify({'message': 'Usuário deletado com sucesso!'}), 200
+
+###### FUNCOES ######
 @app.route('/funcoes', methods=['GET'])
 def funcoes():
     response = None
@@ -116,6 +186,7 @@ def funcoes():
         response_dict = [{'Função': j[1], 'Nível Acesso': j[2]}  for i,j in enumerate(response)]
     return jsonify(response_dict), 200
 
+###### SALAS ######
 @app.route('/salas', methods=['GET'])
 def salas():
     with db_connection_handler as db:
@@ -145,9 +216,15 @@ def adicionar_sala():
         return jsonify({"error": "Formato de payload inválido. Campos faltantes: {}".format(list)}), 400
         
     with db_connection_handler as db:
-        db.begin() if not db.in_transaction() else None
-        db.execute(repository_sala.insert_room(dados_sala))
-        db.commit()
+        try:
+            db.begin() if not db.in_transaction() else None
+            db.execute(repository_sala.insert_room(dados_sala))
+            db.commit()
+        except Exception as e:
+            print(e)
+            db.rollback()
+            return {'Error': 'Houve um erro no banco de dados.'}, 400
+        
         
     return jsonify({'message': 'Sala cadastrada com sucesso!', 'dados': dados_sala}), 200
 
@@ -156,29 +233,52 @@ def alterar_sala(id):
     if not request.is_json:
         return jsonify({"error": "Formato inválido. Necessário o parâmetro Header = Content-Type: application/json."}), 400
     
-    dados_usuario = request.json
+    dados_sala = request.json
 
-    if len(dados_usuario) != 3:
-        return jsonify({"error": "Formato de payload inválido. Necessário 3 campos no payload (NM_USUARIO, CPF, ID_FUNCAO)."}), 400
+    if len(dados_sala) != 2:
+        return jsonify({"error": "Formato de payload inválido. Necessário 2 campos no payload (DE_SALA, NVL_ACESSO)."}), 400
     
-    if not dados_usuario.get('NM_USUARIO') or not dados_usuario.get('CPF') or not dados_usuario.get('ID_FUNCAO'):
-        list = [x for x in ('NM_USUARIO','CPF','ID_FUNCAO') if x not in dados_usuario]
+    if not dados_sala.get('DE_SALA') or not dados_sala.get('NVL_ACESSO') :
+        list = [x for x in ('DE_SALA','NVL_ACESSO') if x not in dados_sala]
 
         return jsonify({"error": "Formato de payload inválido. Campos faltantes: {}".format(list)}), 400
 
-    dados_usuario['ID'] = id
+    dados_sala['ALTERED_AT'] = datetime.now()
+    dados_sala['ID'] = id
 
     with db_connection_handler as db:
-        db.begin() if not db.in_transaction() else None
-        result = db.execute(repository_usuario.alter_user(dados_usuario))
-        db.commit()
+        try:
+            db.begin() if not db.in_transaction() else None
+            result = db.execute(repository_sala.alter_room(dados_sala))
+            db.commit()
+        except Exception as e:
+            print(e)
+            db.rollback()
+            return {'Error': 'Houve um erro no banco de dados.'}, 400
     
     if result.rowcount == 0:
-        return jsonify({'error': 'ID_USUARIO não encontrado.', 'dados': dados_usuario}), 404
+        return jsonify({'error': 'ID_SALA não encontrado.'}), 404
     else:
-        return jsonify({'message': 'Usuário alterado com sucesso!', 'dados': dados_usuario}), 200
+        return jsonify({'message': 'Sala alterada com sucesso!', 'dados': dados_sala}), 200
 
+@app.route('/salas/delete/<id>', methods=['POST'])
+def deletar_sala(id):
+    with db_connection_handler as db:
+        try:
+            db.begin() if not db.in_transaction() else None
+            result = db.execute(repository_sala.delete_room(id))
+            db.commit()
+        except Exception as e:
+            print(e)
+            db.rollback()
+            return {'Error': 'Houve um erro no banco de dados.'}, 400
+    
+    if result.rowcount == 0:
+        return jsonify({'error': f'ID_SALA {id} não encontrado.'}), 404
+    else:
+        return jsonify({'message': 'Sala deletada com sucesso!'}), 200
 
+###### INVENTÁRIO ######
 @app.route('/inventario', methods=['GET'])
 def inventario():
     with db_connection_handler as db:
@@ -213,7 +313,56 @@ def adicionar_item():
         db.execute(repository_inventory.insert_item(dados_item))
         db.commit()
         
-    return jsonify({'message': 'Usuário cadastrado com sucesso!', 'dados': dados_item}), 200
+    return jsonify({'message': 'Recurso cadastrado com sucesso!', 'dados': dados_item}), 200
+
+@app.route('/inventario/alter/<id>', methods=['POST'])
+def alterar_item(id):
+    if not request.is_json:
+        return jsonify({"error": "Formato inválido. Necessário o parâmetro Header = Content-Type: application/json."}), 400
+    
+    dados_item = request.json
+
+    if len(dados_item) != 3:
+        return jsonify({"error": "Formato de payload inválido. Necessário 3 campos no payload (DE_RECURSO,NR_SERIE,ID_SALA)."}), 400
+    
+    if not dados_item.get('DE_RECURSO') or not dados_item.get('NR_SERIE') or not dados_item.get('ID_SALA') :
+        list = [x for x in ('DE_RECURSO','NR_SERIE','ID_SALA') if x not in dados_item]
+        return jsonify({"error": "Formato de payload inválido. Campos faltantes: {}".format(list)}), 400
+
+    dados_item['ALTERED_AT'] = datetime.now()
+    dados_item['ID'] = id
+
+    with db_connection_handler as db:
+        try:
+            db.begin() if not db.in_transaction() else None
+            result = db.execute(repository_inventory.alter_item(dados_item))
+            db.commit()
+        except Exception as e:
+            print(e)
+            db.rollback()
+            return {'Error': 'Houve um erro no banco de dados.'}, 400
+    
+    if result.rowcount == 0:
+        return jsonify({'error': 'ID_RECURSO não encontrado.'}), 404
+    else:
+        return jsonify({'message': 'Recurso alterado com sucesso!', 'dados': dados_item}), 200
+
+@app.route('/inventario/delete/<id>', methods=['POST'])
+def deletar_item(id):
+    with db_connection_handler as db:
+        try:
+            db.begin() if not db.in_transaction() else None
+            result = db.execute(repository_inventory.delete_item(id))
+            db.commit()
+        except Exception as e:
+            print(e)
+            db.rollback()
+            return {'Error': 'Houve um erro no banco de dados.'}, 400
+    
+    if result.rowcount == 0:
+        return jsonify({'error': f'ID_RECURSO {id} não encontrado.'}), 404
+    else:
+        return jsonify({'message': 'Recurso deletado com sucesso!'}), 200
 
 
 if __name__ == '__main__':
--- src/models/database/repository.py
+++ src/models/database/repository.py
@@ -4,12 +4,57 @@ class RepositoryUsuario:
     def select_all():
         return text('SELECT * FROM TBL_USUARIO')
 
+    def select_user(nm_usuario:str):
+        return text("""
+            SELECT tu.NM_USUARIO, tu.SENHA, tf.NVL_ACESSO 
+            FROM TBL_USUARIO tu 
+            INNER JOIN TBL_FUNCAO tf ON tu.ID_FUNCAO = tf.ID_FUNCAO  
+            WHERE tu.NM_USUARIO = '{}'""".format(nm_usuario.upper())
+        )
+
     def insert_user(dados:dict):
         query = text("INSERT INTO tbl_usuario (NM_USUARIO, CPF, SENHA, ID_FUNCAO) VALUES ('%s', '%s', '%s', %s)" % tuple(x for x in dados.values()))
         return query
 
     def alter_user(dados:dict):
-        query = text("UPDATE TBL_USUARIO SET NM_USUARIO = '%s', CPF = '%s', ID_FUNCAO = %s WHERE ID_USUARIO = %s" % tuple(x for x in dados.values()))
+        if dados['SENHA']:
+            query = text("""
+                UPDATE TBL_USUARIO 
+                SET 
+                    NM_USUARIO = '{NM_USUARIO}', 
+                    CPF = '{CPF}', 
+                    ID_FUNCAO = {ID_FUNCAO},
+                    ALTERED_AT = '{ALTERED_AT}',
+                    SENHA = '{SENHA}'
+                WHERE ID_USUARIO = {ID_USUARIO}""".format(
+                    NM_USUARIO = dados['NM_USUARIO'],
+                    CPF = dados['CPF'],
+                    ID_FUNCAO = dados['ID_FUNCAO'],
+                    ALTERED_AT = dados['ALTERED_AT'],
+                    SENHA = dados['SENHA'],
+                    ID_USUARIO = dados['ID']
+                )
+            )
+        else:
+            query = text("""
+                UPDATE TBL_USUARIO 
+                SET 
+                    NM_USUARIO = '{NM_USUARIO}', 
+                    CPF = '{CPF}', 
+                    ID_FUNCAO = {ID_FUNCAO},
+                    ALTERED_AT = '{ALTERED_AT}'
+                WHERE ID_USUARIO = {ID_USUARIO}""".format(
+                    NM_USUARIO = dados['NM_USUARIO'],
+                    CPF = dados['CPF'],
+                    ID_FUNCAO = dados['ID_FUNCAO'],
+                    ALTERED_AT = dados['ALTERED_AT'],
+                    ID_USUARIO = dados['ID']
+                ) 
+            )
+        return query
+
+    def delete_user(id):
+        query = text("DELETE FROM TBL_USUARIO WHERE ID_USUARIO = %s" % id)
         return query
 
 class RepositoryFuncao:
@@ -24,10 +69,39 @@ class RepositorySala:
         query = text("INSERT INTO tbl_sala (DE_SALA, NVL_ACESSO) VALUES ('%s', %s)" % tuple(x for x in dados.values()))
         return query
 
+    def alter_room(dados:dict):
+        query = text("""
+            UPDATE TBL_SALA
+            SET 
+                DE_SALA = '%s', 
+                ID_SALA = %s,
+                NVL_ACESSO = '%s'
+            WHERE ID_SALA = %s""" % tuple(x for x in dados.values()))
+        return query
+
+    def delete_room(id):
+        query = text("DELETE FROM TBL_SALA WHERE ID_SALA = %s" % id)
+        return query
+
 class RepositoryInventory:
     def select_all():
         return text('SELECT * FROM TBL_INVENTARIO')
     
     def insert_item(dados:dict):
         query = text("INSERT INTO tbl_inventario (DE_RECURSO, NR_SERIE, ID_SALA) VALUES ('%s', '%s', %s)" % tuple(x for x in dados.values()))
+        return query
+    
+    def alter_item(dados:dict):
+        query = text("""
+            UPDATE TBL_INVENTARIO
+            SET 
+                DE_RECURSO = '%s', 
+                NR_SERIE = '%s',
+                ID_SALA = '%s',
+                ALTERED_AT = '%s'
+            WHERE ID_RECURSO = %s""" % tuple(x for x in dados.values()))
+        return query
+    
+    def delete_item(id):
+        query = text("DELETE FROM TBL_INVENTARIO WHERE ID_RECURSO = %s" % id)
         return query
